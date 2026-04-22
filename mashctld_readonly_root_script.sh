bf9c39cd8020ccfb3089d3a3dd7c8d495782d669
Sven Geggus <sven-git@geggus.net>
2026-04-22 20:21:21 +0200

    Change script for native overlayroot package available in Raspberry pi OS Trixie

--- mashctld_readonly_root_script.sh
+++ mashctld_readonly_root_script.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
+#
+# Persist configuration in readonly mode for
+# Raspberry Pi OS Trixie overlayroot system
+#
+# Do this by:
+# mounting fs rw, copy mashctld.conf and mounting fs rw again
+#
 
 TMP_CFG=/tmp/mashctld.conf
 TARGET_CFG=/etc/mashctld.conf
+
+# exit if not mounted ro
+if ! mount |grep -q -e '^/dev/mmcblk0p2.* (ro'; then
+  exit 0
+fi
 
 # make shure we run as root
 if [ $(id -u) != 0 ]; then
   exec sudo $0
 fi
 
-mount -o remount,rw /
-cp $TMP_CFG $TARGET_CFG
-mount -o remount,ro /
+mount -o remount,rw /dev/mmcblk0p2
+cp $TMP_CFG /media/root-ro$TARGET_CFG
+mount -o remount,ro /dev/mmcblk0p2
 
-
-  
